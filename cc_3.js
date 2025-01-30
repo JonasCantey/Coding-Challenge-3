f0e1e706a0a61b27c2e1f5abf3a42750fa751d91
Jonas <jonashooncantey@gmail.com>
2025-01-29 20:40:27 -0500

    Task 1 - Product Price Management

---
+++ cc_3.js
@@ -0,0 +1,9 @@
+// Task 1
+console.log("Task 1")
+
+let prices = [300, 400, 500, 600, 700]  //Declaring array of prices
+prices.push (800)                       //Adds price of 800 to the array
+prices.shift()                          //Removes the first item in the array
+
+console.log(prices)
+
